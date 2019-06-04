626bac91dff63eba5ede995ab228876e208acff4
Mark Harwood <m-harwood@hotmail.com>
2019-06-04 10:12:21 +0000

    Beautified code and removed unneeded parts, added more content to the readme and started testing site

--- README.md
+++ README.md
@@ -1,45 +1,71 @@
-<h1>**Simon!**</h1>
+# **Simon!**
 
 My project is the simon game as developed by Hasbro. I have made it as accurate to the original game as I think I can and am very happy with the outcome.
-I have added wiki links to the game and to hasbro and also to my GitHub. I added a way to contact me with ideas or anything 
+I have added wiki links to the game and to hasbro and also to my GitHub.
 
-**UX**
+# **UX**
 
 Use this section to provide insight into your UX process, focusing on who this website is for, what it is that they want to achieve and how your project is the best way to help them achieve these things.
 
 In particular, as part of this section we recommend that you provide a list of User Stories, with the following general structure:
 
++ User 1 : I would like to play a game of simon, so I load up the site and try to get to 20 to win.
++ User 2 : I would like to know more about the script to be able to run a simon game, so I enter dev mode to see the changes in action.
++ User 3
+
 As a user type, I want to perform an action, so that I can achieve a goal.
 This section is also where you would share links to any wireframes, mockups, diagrams etc. that you created as part of the design process. These files should themselves either be included in the project itself (in an separate directory), or just hosted elsewhere online and can be in any format that is viewable inside the browser.
 
-**Features**
+# **Features**
 
 In this section, you should go over the different parts of your project, and describe each in a sentence or so.
 
 Existing Features
-Feature 1 - allows users X to achieve Y, by having them fill out Z
-...
+Feature 1 - allows users to play a game of simon up to the limit of 20 turns starting at the same point if they get it wrong.
+Feature 2 - allows users to play a game of simon up to the limit of 20 turns but starting again if they get a turn wrong.
+
 For some/all of your features, you may choose to reference the specific project files that implement them, although this is entirely optional.
 
-In addition, you may also use this section to discuss plans for additional features to be implemented in the future:
+In the future I would like to implement different games to allow users to have a small selection to play.
 
-**Features Left to Implement**
+# **Features Left to Implement**
 
-Another feature idea
-Technologies Used
+I would like varied games form hasbro to be played, also allow users to make there own game levels/difficulties.
+
+# **Technologies**
+
++ HTML5
++ CSS
++ JS
++ Bootstrap
++ JQuery
+
 In this section, you should mention all of the languages, frameworks, libraries, and any other tools that you have used to construct this project. For each, provide its name, a link to its official site and a short sentence of why it was used.
 
-**JQuery**
-
 The project uses JQuery to simplify DOM manipulation.
-Testing
+
+### Testing
+
+I tested the JS using [https://codebeautify.org/jsvalidate](https://codebeautify.org/jsvalidate) and the HTML using [https://validator.w3.org/#validate_by_input](https://validator.w3.org/#validate_by_input).
+This allowed me to clean up the code to make it more compact and to fix some minor errors that I would not have noticed if I didnt use these services.
+I then used [https://jsbin.com/?html,css,js,output](https://jsbin.com/?html,css,js,output) which allowed me to run all the HTML, CSS & JS to make sure the site was functioning as intended.
+
+
+The site was tested on multiple browsers and systems such as
+ + Internet Explorer
+ + Chrome
+ + Brave Browser
+ + iphone 8
+ + LG G6
+ + Samsung Galaxy S8+
+
 In this section, you need to convince the assessor that you have conducted enough testing to legitimately believe that the site works well. Essentially, in this part you will want to go over all of your user stories from the UX section and ensure that they all work as intended, with the project providing an easy and straightforward way for the users to achieve their goals.
 
 Whenever it is feasible, prefer to automate your tests, and if you've done so, provide a brief explanation of your approach, link to the test file(s) and explain how to run them.
 
 For any scenarios that have not been automated, test the user stories manually and provide as much detail as is relevant. A particularly useful form for describing your testing process is via scenarios, such as:
 
-**Contact form:**
+# **Contact form:**
 
 Go to the "Contact Us" page
 Try to submit the empty form and verify that an error message about the required fields appears
@@ -51,7 +77,7 @@ You should also mention in this section any interesting bugs or problems you dis
 
 If this section grows too long, you may want to split it off into a separate file and link to it from here.
 
-**Deployment**
+# **Deployment**
 
 This section should describe the process you went through to deploy the project to a hosting platform (e.g. GitHub Pages or Heroku).
 
@@ -62,11 +88,11 @@ Different configuration files?
 Separate git branch?
 In addition, if it is not obvious, you should also describe how to run your code locally.
 
-**Credits**
+# **Credits**
 
-Content
+### Content
 The text for section Y was copied from the Wikipedia article Z
-Media
+### Media
 The photos used in this site were obtained from the Hasbro Wiki.
-Acknowledgements
-I received inspiration for this project from kellyking and Code Planet who had a nice, simple tutorials which I based the intial design from. <a href="https://codeplanet.io/building-simon-says-javascript/">https://codeplanet.io/building-simon-says-javascript/</a>
+### Acknowledgements
+I received inspiration for this project from kellyking and Code Planet who had a nice, simple tutorials which I based the intial design from. [https://codeplanet.io/building-simon-says-javascript/](https://codeplanet.io/building-simon-says-javascript/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -24,223 +24,221 @@ const startButton = document.querySelector("#start");
 /*------------------------------------------ Events for On, Strict mode and win  */
 
 strictButton.addEventListener("click", (event) => {
-    if (strictButton.checked == true) {
-        strict = true;
-    }
-    else {
-        strict = false;
-    }
+ if (strictButton.checked === true) {
+  strict = true;
+ } else {
+  strict = false;
+ }
 });
 
 onButton.addEventListener("click", (event) => {
-    if (onButton.checked == true) {
-        on = true;
-        turnCounter.innerHTML = "-";
-    }
-    else {
-        on = false;
-        turnCounter.innerHTML = "";
-        clearColor();
-        clearInterval(intervalId);
-    }
+ if (onButton.checked === true) {
+  on = true;
+  turnCounter.innerHTML = "-";
+ } else {
+  on = false;
+  turnCounter.innerHTML = "";
+  clearColor();
+  clearInterval(intervalId);
+ }
 });
 
 startButton.addEventListener("click", (event) => {
-    if (on || win) {
-        play();
-    }
+ if (on || win) {
+  play();
+ }
 });
 
 /*------------------------------------------ Functions For Game  */
 
 function play() {
-    win = false;
-    order = [];
-    playerOrder = [];
-    flash = 0;
-    intervalId = 0;
-    turn = 1;
-    turnCounter.innerHTML = 1;
-    good = true;
-    for (var i = 0; i < 20; i++) {
-        order.push(Math.floor(Math.random() * 4) + 1);
-    }
-    compTurn = true;
+ win = false;
+ order = [];
+ playerOrder = [];
+ flash = 0;
+ intervalId = 0;
+ turn = 1;
+ turnCounter.innerHTML = 1;
+ good = true;
+ for (var i = 0; i < 20; i++) {
+  order.push(Math.floor(Math.random() * 4) + 1);
+ }
+ compTurn = true;
 
-    intervalId = setInterval(gameTurn, 800);
+ intervalId = setInterval(gameTurn, 800);
 }
 
 function gameTurn() {
-    on = false;
+ on = false;
 
-    if (flash == turn) {
-        clearInterval(intervalId);
-        compTurn = false;
-        clearColor();
-        on = true;
-    }
+ if (flash == turn) {
+  clearInterval(intervalId);
+  compTurn = false;
+  clearColor();
+  on = true;
+ }
 
-    if (compTurn) {
-        clearColor();
-        setTimeout(() => {
-            if (order[flash] == 1) one();
-            if (order[flash] == 2) two();
-            if (order[flash] == 3) three();
-            if (order[flash] == 4) four();
-            flash++;
-        }, 200);
-    }
+ if (compTurn) {
+  clearColor();
+  setTimeout(() => {
+   if (order[flash] == 1) one();
+   if (order[flash] == 2) two();
+   if (order[flash] == 3) three();
+   if (order[flash] == 4) four();
+   flash++;
+  }, 200);
+ }
 }
 
 function one() {
-  if (noise) {
-    let audio = document.getElementById("clip1");
-    audio.play();
-  }
-  noise = true;
-  green.style.backgroundColor = "#66ff99";
+ if (noise) {
+  let audio = document.getElementById("clip1");
+  audio.play();
+ }
+ noise = true;
+ green.style.backgroundColor = "#66ff99";
 }
 
 function two() {
-  if (noise) {
-    let audio = document.getElementById("clip2");
-    audio.play();
-  }
-  noise = true;
-  red.style.backgroundColor = "#ff8080";
+ if (noise) {
+  let audio = document.getElementById("clip2");
+  audio.play();
+ }
+ noise = true;
+ red.style.backgroundColor = "#ff8080";
 }
 
 function three() {
-  if (noise) {
-    let audio = document.getElementById("clip3");
-    audio.play();
-  }
-  noise = true;
-  yellow.style.backgroundColor = "#ffff80";
+ if (noise) {
+  let audio = document.getElementById("clip3");
+  audio.play();
+ }
+ noise = true;
+ yellow.style.backgroundColor = "#ffff80";
 }
 
 function four() {
-  if (noise) {
-    let audio = document.getElementById("clip4");
-    audio.play();
-  }
-  noise = true;
-  blue.style.backgroundColor = "#8080ff";
+ if (noise) {
+  let audio = document.getElementById("clip4");
+  audio.play();
+ }
+ noise = true;
+ blue.style.backgroundColor = "#8080ff";
 }
 
 function clearColor() {
-  green.style.backgroundColor = "#009933"; // Green
-  red.style.backgroundColor = "#990000"; // Red
-  yellow.style.backgroundColor = "#999900"; // Yellow
-  blue.style.backgroundColor = "#000099"; // Blue
+ green.style.backgroundColor = "#009933"; // Green
+ red.style.backgroundColor = "#990000"; // Red
+ yellow.style.backgroundColor = "#999900"; // Yellow
+ blue.style.backgroundColor = "#000099"; // Blue
 }
 
 function flashColor() {
-  green.style.backgroundColor = "#66ff99"; //Light Green
-  red.style.backgroundColor = "#ff8080"; // Light Red
-  yellow.style.backgroundColor = "#ffff80"; // Light Yellow
-  blue.style.backgroundColor = "#8080ff"; // Light Blue
+ green.style.backgroundColor = "#66ff99"; //Light Green
+ red.style.backgroundColor = "#ff8080"; // Light Red
+ yellow.style.backgroundColor = "#ffff80"; // Light Yellow
+ blue.style.backgroundColor = "#8080ff"; // Light Blue
 }
 
 /*------------------------------------------ Event Listeners  */
 
 green.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(1);
-    check();
-    one();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
+ if (on) {
+  playerOrder.push(1);
+  check();
+  one();
+  if (!win) {
+   setTimeout(() => {
+    clearColor();
+   }, 300);
   }
-})
+ }
+});
 
 red.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(2);
-    check();
-    two();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
+ if (on) {
+  playerOrder.push(2);
+  check();
+  two();
+  if (!win) {
+   setTimeout(() => {
+    clearColor();
+   }, 300);
   }
-})
+ }
+});
 
 yellow.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(3);
-    check();
-    three();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
+ if (on) {
+  playerOrder.push(3);
+  check();
+  three();
+  if (!win) {
+   setTimeout(() => {
+    clearColor();
+   }, 300);
   }
-})
+ }
+});
 
 blue.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(4);
-    check();
-    four();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
+ if (on) {
+  playerOrder.push(4);
+  check();
+  four();
+  if (!win) {
+   setTimeout(() => {
+    clearColor();
+   }, 300);
   }
-})
+ }
+});
 
 /*------------------------------------------ Win state  */
 
 function check() {
-  if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
-    good = false;
+ if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1])
+  good = false;
 
-  if (playerOrder.length == 20 && good) {
-    winGame();
-  }
+ if (playerOrder.length == 20 && good) {
+  winGame();
+ }
 
-  if (good == false) {
-    flashColor();
-    turnCounter.innerHTML = "NO!";
-    setTimeout(() => {
-      turnCounter.innerHTML = turn;
-      clearColor();
+ if (good === false) {
+  flashColor();
+  turnCounter.innerHTML = "NO!";
+  setTimeout(() => {
+   turnCounter.innerHTML = turn;
+   clearColor();
 
-      if (strict) {
-        play();
-      } else {
-        compTurn = true;
-        flash = 0;
-        playerOrder = [];
-        good = true;
-        intervalId = setInterval(gameTurn, 800);
-      }
-    }, 800);
-
-    noise = false;
-  }
-
-  if (turn == playerOrder.length && good && !win) {
-    turn++;
-    playerOrder = [];
+   if (strict) {
+    play();
+   } else {
     compTurn = true;
     flash = 0;
-    turnCounter.innerHTML = turn;
+    playerOrder = [];
+    good = true;
     intervalId = setInterval(gameTurn, 800);
-  }
+   }
+  }, 800);
+
+  noise = false;
+ }
+
+ if (turn == playerOrder.length && good && !win) {
+  turn++;
+  playerOrder = [];
+  compTurn = true;
+  flash = 0;
+  turnCounter.innerHTML = turn;
+  intervalId = setInterval(gameTurn, 800);
+ }
 
 }
 
 function winGame() {
-  flashColor();
-  turnCounter.innerHTML = "WIN!";
-  on = false;
-  win = true;
+ flashColor();
+ turnCounter.innerHTML = "WIN!";
+ on = false;
+ win = true;
 }
